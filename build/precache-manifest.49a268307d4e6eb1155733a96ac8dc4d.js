f74f9e519a990fb4de621e18031f51de44b53cef
mosalaDev <samkyunyudev@gmail.com>
2022-10-22 08:17:50 +0100

    First commit

---
+++ build/precache-manifest.49a268307d4e6eb1155733a96ac8dc4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7509514bada3f461103b55ced7f125c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93bcbb0b1a463b4f2447",
+    "url": "/static/css/main.3ed52446.chunk.css"
+  },
+  {
+    "revision": "cf1fdd9b2f43c99c6ebe",
+    "url": "/static/js/2.3b344e91.chunk.js"
+  },
+  {
+    "revision": "93bcbb0b1a463b4f2447",
+    "url": "/static/js/main.d40bebaa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad17058e0be79216b2a5ad2a66b2769c",
+    "url": "/static/media/nodata.ad17058e.svg"
+  },
+  {
+    "revision": "c7678f08d4ad6bdbc1ec8ef338b38c5d",
+    "url": "/static/media/void.c7678f08.svg"
+  }
+]);
---
+++ build/precache-manifest.932e757d859bf5a413625419578fb360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bcf0c8dc593cd4bc6be8118e654afa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aff8af8aa3970ea8488",
+    "url": "/static/css/main.702167b5.chunk.css"
+  },
+  {
+    "revision": "f9b22cc41de1fa84514b",
+    "url": "/static/js/2.c77fa24a.chunk.js"
+  },
+  {
+    "revision": "1aff8af8aa3970ea8488",
+    "url": "/static/js/main.eb1e3294.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad17058e0be79216b2a5ad2a66b2769c",
+    "url": "/static/media/nodata.ad17058e.svg"
+  },
+  {
+    "revision": "c7678f08d4ad6bdbc1ec8ef338b38c5d",
+    "url": "/static/media/void.c7678f08.svg"
+  }
+]);
